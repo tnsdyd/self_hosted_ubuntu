fe710a01e12d4d951468b140b8268325d3e91aef
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-16 17:30:08 +0900

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-TEST='scp -i /root/id_rsa-11.pem deploy.sh root@10.10.210.11:/root/gittest'
+# TEST='scp -i /root/id_rsa-11.pem deploy.sh root@10.10.210.11:/root/gittest'
 TEST2='ssh -i /root/id_rsa-11.pem root@10.10.210.11'
 
-$TEST
-$TEST2 bash /root/gittest/deploy.sh
+# $TEST
+# $TEST2 bash /root/gittest/deploy.sh
+$TSET2 podman cp /root/gittest/test2.war tomcat:/usr/local/tomcat/webapps/
+$TSET2 podman exec -it tomcat bin/startup.sh
