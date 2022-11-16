545f124c240ddf2c08ac7e0dc214aecf08f71486
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-16 16:31:28 +0900

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -3,7 +3,5 @@
 TEST='scp -i /root/id_rsa-11.pem /root/gittest/deploy.sh root@10.10.210.11:/root/gittest'
 TEST2='ssh -i /root/id_rsa-11.pem root@10.10.210.11'
 
-
-
 $TEST
 $TEST2 bash /root/gittest/deploy.sh
