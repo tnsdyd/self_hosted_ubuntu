dbadecaaae7553461d7bff2ce78ad050f329905c
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-16 16:34:40 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TEST='ssh -i /root/id_rsa-11.pem root@10.10.210.21'
+TEST2='scp -i /root/id_rsa-11.pem /root/gittest/deploy.sh root@10.10.210.21:/root/gittest'
+
+podman cp /root/gittest/test2.war tomcat:/usr/local/tomcat/webapps/
+podman exec -it tomcat bin/startup.sh
