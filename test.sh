99aaf0bf978b6e23cb61500336675aee5a7ab7e4
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-16 15:36:58 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEST='ssh -i /root/id_rsa.pem root@10.10.210.21'
-TEST2='scp -i /root/id_rsa.pem /root/gittest/README.md root@10.10.210.21:/root/gittest'
+TEST2='scp -i /root/id_rsa.pem /root/gittest/test2.war root@10.10.210.21:/root/gittest'
 
 $TEST2
 $TEST ls -al /root/gittest/ 
