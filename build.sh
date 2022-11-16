5cc6e6d3a3e26c369ba91eb5789967a29b176888
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-16 15:42:05 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd mvntest
+cd /root/mvntest
 mvn archetype:generate -DinteractiveMode=false  -DgroupId=test -DartifactId=test2 -DarchetypeArtifactId=maven-archetype-webapp
 cd /root/mvntest/test2
 mvn install
