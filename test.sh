368a71112536af38fcf253abaf557c8781af8d08
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-16 13:02:11 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 
 TEST='ssh -i /root/id_rsa.pem root@10.10.210.21'
 
+
+scp -i /root/id_rsa.pem /root/gittest/* root@10.10.210.21 
 $TEST ls -al
-$TEST oc login -u admin -p welcome1!
-$TEST oc get node
-$TEST ip a
