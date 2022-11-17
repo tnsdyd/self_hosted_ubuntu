17c4d5536b4ff5e9f98f07186fa6f4b19a1708cb
tnsdyd <99467409+tnsdyd@users.noreply.github.com>
2022-11-17 09:13:25 +0900

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /root/mvntest
-mvn archetype:generate -DinteractiveMode=false  -DgroupId=test -DartifactId=test2 -DarchetypeArtifactId=maven-archetype-webapp
-cd /root/mvntest/test2
-mvn install
